f464f26d6a4fc41f32098d6c2a694afb2c972f78
Erick Yael Martinez Garcia <168610567+ErickYaelMartinezGarcia@users.noreply.github.com>
2025-02-05 18:55:38 +0000

    CREACION DE LOGICA

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ let amigos = [];
 function addFriend(txt){
     amigos.push(txt);
 };
-function validateFriend(input){
-    if( input == ''){
-        return alert('Por favor, inserte un nombre.');
-    }
-        addFriend(input);
-         return input = '';
-};
+
 function agregarAmigo(txt){
-    let inputValue= document.getElementById(txt);
-        validateFriend(inputValue);
+    let inputValue = document.getElementById(txt).value;
+        if(inputValue == ''){
+           return  alert('Por favor, inserte un nombre.');
+        }
+        addFriend(inputValue); 
+        document.getElementById(txt).value = "";
+        showFriends();
+        
+}
+function showFriends(){
+    let listContainer = document.getElementById('listaAmigos')
+        listContainer.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++){
+        listContainer.innerHTML += `<li>${amigos[i]}</li>`
+    }
 }
 
+function
